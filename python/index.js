5aff20877b8144cdbe31ec307807483751d7710c
Panyut Sriwirote <mildpanyut@gmail.com>
2023-03-09 14:11:35 +0700

    simplify handling of stdout

--- python/index.js
+++ python/index.js
@@ -1,25 +1,22 @@
-$(function() {
+$(() => {
     const editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
     editor.session.setMode("ace/mode/python");
     const output = $("#output");
-    async function load_interpreter() {
+    const load_interpreter = async () => {
         output.text("กำลังเตรียมระบบไพธอน...");
-        const interpreter = await loadPyodide();
-        interpreter.runPython("import sys, io;sys.stdout = io.StringIO()");
+        const interpreter = await loadPyodide({"stdout": (msg) => {output.append(msg + '\n')}});
         output.text("ไพธอนพร้อมใช้งาน!");
         return interpreter;
     }
     const promised_interpreter = load_interpreter();
-    $("#run").click(async function() {
-        output.text("กำลังรัน...");
+    $("#run").click(async () => {
+        output.text('');
         const interpreter = await promised_interpreter;
         try {
             await interpreter.runPythonAsync(editor.getValue());
-            output.text(interpreter.runPython("sys.stdout.getvalue()"));
-            interpreter.runPython("sys.stdout.close();sys.stdout = io.StringIO()");
         } catch (err) {
-            output.text(err);
+            output.append(err);
         }
     });
 });
